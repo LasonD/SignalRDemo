0f5787e91e2840d0e25fd2304f744498a39de2ee
Oleksandr Barylo <sbarylo20023@gmail.com>
2023-04-17 21:58:21 +0300

    Some UI adjustments

--- signalr-demo/src/app/declarations-list/declaration/declaration.component.ts
+++ signalr-demo/src/app/declarations-list/declaration/declaration.component.ts
@@ -4,25 +4,47 @@ import { Declaration } from "../../models/declaration.model";
 @Component({
   selector: 'app-declaration',
   templateUrl: './declaration.component.html',
-  styleUrls: ['./declaration.component.scss']
+  styleUrls: ['./declaration.component.scss'],
 })
 export class DeclarationComponent {
   @Input() declaration!: Declaration;
+  @Input() jurisdictions: string[] = ['GB', 'BE', 'NL'];
   editMode: boolean = false;
 
   toggleEditMode(): void {
     this.editMode = !this.editMode;
   }
 
-  saveDeclaration(): void {
+  cancelEdit(): void {
+    this.editMode = false;
+  }
+
+  saveChanges(): void {
     // Implement your save functionality here
     this.toggleEditMode();
   }
 
-  getTransparentBackgroundColor(): string {
+  getBackgroundColor(): string {
     const color = this.declaration.displayColor;
-    const opacity = 0.2; // Adjust the value between 0 and 1 to change the opacity.
-    const rgbaColor = color.replace('rgb', 'rgba').replace(')', `, ${opacity})`);
-    return rgbaColor;
+    const rgba = this.colorToRGBA(color, 0.2);
+    return rgba;
+  }
+
+  colorToRGBA(color: string, opacity: number): string {
+    const tempElement = document.createElement('div');
+    tempElement.style.backgroundColor = color;
+    document.body.appendChild(tempElement);
+    const style = getComputedStyle(tempElement);
+    const rgb = style.backgroundColor;
+    document.body.removeChild(tempElement);
+
+    const regex = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/.exec(rgb);
+    if (regex) {
+      const r = parseInt(regex[1]);
+      const g = parseInt(regex[2]);
+      const b = parseInt(regex[3]);
+      return `rgba(${r}, ${g}, ${b}, ${opacity})`;
+    }
+    return color;
   }
 }
